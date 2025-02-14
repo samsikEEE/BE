fba8e8763e4990c1e0743674d84dcfc3bfe93ae6
YoonJeongyoung <dbswjddud100@naver.com>
2025-02-14 14:59:32 +0900

    feature: 주문 조회 서비스 중간 저장

--- src/main/java/com/sprta/samsike/application/service/Orderservice.java
+++ src/main/java/com/sprta/samsike/application/service/Orderservice.java
@@ -1,8 +1,11 @@
 package com.sprta.samsike.application.service;
 
 import com.sprta.samsike.application.dto.order.OrderResponseDto;
+import com.sprta.samsike.domain.member.Member;
+import com.sprta.samsike.domain.member.MemberRoleEnum;
 import com.sprta.samsike.domain.order.Order;
 import com.sprta.samsike.infrastructure.persistence.jpa.OrderRepository;
+import com.sprta.samsike.infrastructure.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -16,19 +19,22 @@ public class Orderservice {
 
     private final OrderRepository orderRepository;
 
-//    public Page<OrderResponseDto> getOrders(UserDetailsImpl userDetails, int i, int size, String sortBy, boolean isAsc) {
-//        Sort.Direction direction = isAsc ? Sort.Direction.ASC : Sort.Direction.DESC;
-//        Pageable pageable = PageRequest.of(page, size, Sort.by(direction, sortBy));
-//
-//        Page<Order> orderList;
-//        if (userDetails.hasRole("ROLE_MASTER") && userDetails.hasRole("ROLE_MANAGER")) {
-//            orderList = orderRepository.findAll(pageable);
-//        } else if (userDetails.hasRole("ROLE_OWNER")) {
-//            orderList = orderRepository.findByRestaurantOwnerUsername(userDetails.getUsername(), pageable);
-//        } else {
-//            orderList = orderRepository.findByMemberUsername(userDetails.getUsername(), pageable);
-//        }
-//
-//        return orderList.map(OrderResponseDto::new);
-//    }
+    public Page<OrderResponseDto> getOrders(Member member, int page, int size, String sortBy, boolean isAsc) {
+        Sort.Direction direction = isAsc ? Sort.Direction.ASC : Sort.Direction.DESC;
+        Sort sort = Sort.by(direction, sortBy);
+        Pageable pageable = PageRequest.of(page, size, sort);
+
+        MemberRoleEnum memberRoleEnum = member.getRole();
+
+        Page<Order> orderList;
+        if (memberRoleEnum == MemberRoleEnum.MASTER || memberRoleEnum == MemberRoleEnum.MANAGER) {
+            orderList = orderRepository.findAll(pageable);
+        } else if (memberRoleEnum == MemberRoleEnum.OWNER) {
+            orderList = orderRepository.findByMemberUsername(member.getUsername(), pageable);
+        } else {
+            orderList = orderRepository.findByMemberUsername(member.getUsername(), pageable);
+        }
+
+        return orderList.map(OrderResponseDto::new);
+    }
 }
--- src/main/java/com/sprta/samsike/presentation/controller/OrderController.java
+++ src/main/java/com/sprta/samsike/presentation/controller/OrderController.java
@@ -3,9 +3,11 @@ package com.sprta.samsike.presentation.controller;
 import com.sprta.samsike.application.dto.order.OrderResponseDto;
 import com.sprta.samsike.application.dto.response.ApiResponseDTO;
 import com.sprta.samsike.application.service.Orderservice;
+import com.sprta.samsike.infrastructure.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,15 +21,15 @@ public class OrderController {
     private final Orderservice orderService;
 
     // 주문 전체 조회 (페이지네이션 + 정렬)
-//    @GetMapping
-//    public ResponseEntity<ApiResponseDTO<Page<OrderResponseDto>>> getOrders(
-//            @RequestParam(value = "page", defaultValue = "1") int page,
-//            @RequestParam(value = "size", defaultValue = "10") int size,
-//            @RequestParam(value = "sortBy", defaultValue = "createdAt") String sortBy,
-//            @RequestParam(value = "isAsc", defaultValue = "true") boolean isAsc,
-//            @AuthenticationPrincipal UserDetailsImpl userDetails) {
-//
-//        Page<OrderResponseDto> orders = orderService.getOrders(userDetails, page - 1, size, sortBy, isAsc);
-//        return ResponseEntity.ok(ApiResponseDTO.success(orders));
-//    }
+    @GetMapping
+    public ResponseEntity<ApiResponseDTO<Page<OrderResponseDto>>> getOrders(
+            @RequestParam(value = "page", defaultValue = "1") int page,
+            @RequestParam(value = "size", defaultValue = "10") int size,
+            @RequestParam(value = "sortBy", defaultValue = "createdAt") String sortBy,
+            @RequestParam(value = "isAsc", defaultValue = "true") boolean isAsc,
+            @AuthenticationPrincipal UserDetailsImpl userDetails) {
+
+        Page<OrderResponseDto> orders = orderService.getOrders(userDetails.getMember(), page - 1, size, sortBy, isAsc);
+        return ResponseEntity.ok(ApiResponseDTO.success(orders));
+    }
 }
