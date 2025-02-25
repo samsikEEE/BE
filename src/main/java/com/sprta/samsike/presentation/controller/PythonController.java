56d93a519ae5a11030b989e2761de4f3004be5f3
Kim jisu <jisu0259@naver.com>
2025-02-25 15:35:40 +0900

    Fix: @ResponseBody

--- src/main/java/com/sprta/samsike/presentation/controller/PythonController.java
+++ src/main/java/com/sprta/samsike/presentation/controller/PythonController.java
@@ -3,6 +3,7 @@ package com.sprta.samsike.presentation.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
 
 @Controller
@@ -17,6 +18,7 @@ public class PythonController {
     }
 
     @GetMapping("/python-result")
+    @ResponseBody
     public String getPythonResult() {
         String pythonApiUrl = "http://localhost:5000/run-python"; // Flask API URL
         return restTemplate.getForObject(pythonApiUrl, String.class);
